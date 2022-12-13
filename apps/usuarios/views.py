74ace4068be9bec22b8a4a0c3f431416d19dca61
bielmorato <bielmorato09@gmail.com>
2022-12-13 19:01:02 -0300

    projeto

---
+++ apps/usuarios/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+from receitas.models import Receita
+
+
+def cadastro(request):
+    """Cadastra uma nova pesosa no sistema"""
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if campo_vazio(nome):
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if campo_vazio(email):
+            print('O campo email não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            messages.error(request, 'As senhas não são iguais')
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Usuario já castrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        messages.success(request, 'cadastro realizado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    """realiza o login de uma pessoa no sistema"""
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        print(email, senha)
+        if email == "" or senha == "":
+            print('os campos email e senha não podem ser vazios')
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            print(nome)
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request,user)
+                print('login realizado com sucesso')
+            return redirect('dashboard')
+    return render(request, 'usuarios/login.html')
+
+def logout(request):
+    """Efetua logout da conta"""
+    auth.logout(request)
+    return redirect('index') 
+
+def dashboard(request):
+    """Se o usuario estiver autenticado ele filtra todas as receitas do usuario
+    e exibe em um dashboard"""
+    if request.user.is_authenticated:
+        id = request.user.id
+        receitas = Receita.objects.order_by('-data_receita').filter(pessoa=id)
+        dados = {
+            'receitas': receitas
+        }
+        return render(request, 'usuarios/dashboard.html', dados)
+    else:
+        return redirect('index')
+
+def campo_vazio(campo):
+    return not campo.strip()
